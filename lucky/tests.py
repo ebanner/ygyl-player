9020e3b87a23e78a1f52b4732019ea4bcd39957f
Edward Banner <edward.banner@gmail.com>
2018-03-05 21:06:27 -0800

    Add tests

--- lucky/tests.py
+++ lucky/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+class LuckyIndexViewTests(TestCase):
+    def test_webm(self):
+        url = reverse('lucky:index')
+        response = self.client.get(url)
+        html = response.content.decode()
+        self.assertIn('webm', html)
+
+    def test_video(self):
+        url = reverse('lucky:index')
+        response = self.client.get(url)
+        html = response.content.decode()
+        self.assertIn('video', html)
